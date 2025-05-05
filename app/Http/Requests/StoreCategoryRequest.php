1d6a5b19d94c3eaad621330ade12163e07839773
yuuhei-koutoku <yhkt.0627@gmail.com>
2025-05-05 13:03:36 +0900

    category new registration process 2

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -22,7 +22,8 @@ class StoreCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'        => 'required|max:50',
+            'description' => 'required',
         ];
     }
 }
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -10,14 +10,22 @@
                     <div class="p-2 w-full">
                         <div class="relative">
                             <label for="name" class="leading-7 text-sm text-gray-600">カテゴリー名</label>
-                            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <input type="text" id="name" name="name" value="{{ old('name') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
+                        {{-- カテゴリー名のバリデーションエラーメッセージの表示 --}}
+                        @error('name')
+                            <div class="alert alert-danger text-red-700">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="p-2 w-full">
                         <div class="relative">
                             <label for="description" class="leading-7 text-sm text-gray-600">カテゴリー説明文</label>
-                            <textarea id="description" name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                            <textarea id="description" name="description" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ old('description') }}</textarea>
                         </div>
+                        {{-- カテゴリー説明文のバリデーションエラーメッセージの表示 --}}
+                        @error('description')
+                            <div class="alert alert-danger text-red-700">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="p-2 w-full">
                         <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
