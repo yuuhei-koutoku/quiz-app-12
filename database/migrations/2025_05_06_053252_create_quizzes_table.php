56ed79d851ea595aa19b664910f367e40f18149c
yuuhei-koutoku <yhkt.0627@gmail.com>
2025-05-06 14:42:18 +0900

    create quizzes table

--- database/migrations/2025_05_06_053252_create_quizzes_table.php
+++ database/migrations/2025_05_06_053252_create_quizzes_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')
+                ->constrained()       // categoriesテーブルのidカラムと紐づける
+                ->onUpdate('cascade') // 親テーブルのレコードが更新されたとき、同時に更新される
+                ->onDelete('cascade') // 親テーブルのレコードが削除されたとき、同時に削除される
+                ->comment('カテゴリーID');
+            $table->text('question')->comment('問題文');
+            $table->text('explanation')->comment('解説');
             $table->timestamps();
         });
     }
