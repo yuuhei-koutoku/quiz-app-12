bc347dbc363334f4f5cf11bc8bde95a8163b06ec
yuuhei-koutoku <yhkt.0627@gmail.com>
2025-05-10 11:20:57 +0900

    quiz list display 2

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -41,43 +41,47 @@
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-5 mx-auto">
             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-                <table class="table-auto w-full text-left whitespace-no-wrap">
-                    <thead>
-                        <tr>
-                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ID</th>
-                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">クイズ問題文</th>
-                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">更新日時</th>
-                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">編集</th>
-                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">削除</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($quizzes as $quiz)
+                @if (count($quizzes) === 0)
+                    <p class="text-center">クイズがまだ登録されていません</p>
+                @else
+                    <table class="table-auto w-full text-left whitespace-no-wrap">
+                        <thead>
                             <tr>
-                                <td class="px-4 py-3">{{ $quiz->id }}</td>
-                                {{-- 問題文：はじめの10文字だけを表示 --}}
-                                <td class="px-4 py-3">
-                                    {{ Str::length($quiz->question) > 10 ? mb_substr($quiz->question, 0, 10) . '...' : $quiz->question }}
-                                </td>
-                                <td class="px-4 py-3">{{ $quiz->updated_at }}</td>
-                                <td class="px-4 py-3 text-lg text-gray-900">
-                                    <button
-                                        {{-- クイズ編集画面に遷移 --}}
-                                        onclick="location.href='{{ route('admin.categories.edit', ['categoryId' => $category->id]) }}'"
-                                        class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                                        編集
-                                    </button>
-                                </td>
-                                <td class="px-4 py-3 text-lg text-gray-900">
-                                    <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                                        削除
-                                    </button>
-                                </td>
-                                </td>
+                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ID</th>
+                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">クイズ問題文</th>
+                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">更新日時</th>
+                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">編集</th>
+                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">削除</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($quizzes as $quiz)
+                                <tr>
+                                    <td class="px-4 py-3">{{ $quiz->id }}</td>
+                                    {{-- 問題文：はじめの10文字だけを表示 --}}
+                                    <td class="px-4 py-3">
+                                        {{ Str::length($quiz->question) > 10 ? mb_substr($quiz->question, 0, 10) . '...' : $quiz->question }}
+                                    </td>
+                                    <td class="px-4 py-3">{{ $quiz->updated_at }}</td>
+                                    <td class="px-4 py-3 text-lg text-gray-900">
+                                        <button
+                                            {{-- クイズ編集画面に遷移 --}}
+                                            onclick="location.href='{{ route('admin.categories.edit', ['categoryId' => $category->id]) }}'"
+                                            class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                            編集
+                                        </button>
+                                    </td>
+                                    <td class="px-4 py-3 text-lg text-gray-900">
+                                        <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                            削除
+                                        </button>
+                                    </td>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
             </div>
         </div>
     </section>
