424280ed6d971c50bbcaa0d88a3ebf96a39b937a
yuuhei-koutoku <yhkt.0627@gmail.com>
2025-05-11 14:24:35 +0900

    top display 1

--- resources/views/play/top.blade.php
+++ resources/views/play/top.blade.php
@@ -1,3 +1,38 @@
 <x-play-layout>
-    <h1>プレイヤー画面のトップページです</h1>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap w-full mb-20">
+                <div class="w-full mb-6 lg:mb-0">
+                    <h1 class="text-center sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">カテゴリーを選択してください</h1>
+                    <div class="mx-auto h-1 w-20 bg-indigo-500 rounded"></div>
+                </div>
+            </div>
+            <div class="flex flex-wrap -m-4">
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="block bg-gray-100 p-6 rounded-lg">
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー1</h2>
+                        <p class="leading-relaxed text-base">カテゴリー1説明文カテゴリー1説明文カテゴリー1説明文カテゴリー1説明文カテゴリー1説明文</p>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="block bg-gray-100 p-6 rounded-lg">
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー2</h2>
+                        <p class="leading-relaxed text-base">カテゴリー2説明文カテゴリー2説明文カテゴリー2説明文カテゴリー2説明文カテゴリー2説明文</p>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="block bg-gray-100 p-6 rounded-lg">
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー3</h2>
+                        <p class="leading-relaxed text-base">カテゴリー3説明文カテゴリー3説明文カテゴリー3説明文カテゴリー3説明文カテゴリー3説明文</p>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="#" class="block bg-gray-100 p-6 rounded-lg">
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー4</h2>
+                        <p class="leading-relaxed text-base">カテゴリー4説明文カテゴリー4説明文カテゴリー4説明文カテゴリー4説明文カテゴリー4説明文</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-play-layout>
