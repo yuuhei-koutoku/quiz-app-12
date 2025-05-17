a5f024bec930a5ae2977295090df5b80ef6aaf34
yuuhei-koutoku <yhkt.0627@gmail.com>
2025-05-17 11:54:06 +0900

    quiz submission display 2

--- resources/views/play/quizzes.blade.php
+++ resources/views/play/quizzes.blade.php
@@ -1,10 +1,60 @@
 <x-play-layout>
-    <section class="text-gray-600 body-font relative">
+
+    <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">問題</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">問題文が入ります問題文が入ります問題文が入ります</p>
+            <div class="flex flex-col text-center w-full mb-20">
+                <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">問題</h1>
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">問題文です問題文です問題文です</p>
+            </div>
+            <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                <table class="table-auto w-full text-left whitespace-no-wrap">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">番号</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">選択肢</th>
+                            {{-- <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th> --}}
+                            {{-- <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Price</th> --}}
+                            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="px-4 py-3">1</td>
+                            <td class="px-4 py-3">選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です</td>
+                            <td class="w-10 text-center">
+                                <input name="" type="checkbox">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-4 py-3">2</td>
+                            <td class="px-4 py-3">選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です</td>
+                            <td class="w-10 text-center">
+                                <input name="" type="checkbox">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-4 py-3">3</td>
+                            <td class="px-4 py-3">選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です</td>
+                            <td class="w-10 text-center">
+                                <input name="" type="checkbox">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-4 py-3">4</td>
+                            <td class="px-4 py-3">選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です選択肢です</td>
+                            <td class="w-10 text-center">
+                                <input name="" type="checkbox">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                    解答
+                </button>
             </div>
         </div>
     </section>
+
 </x-play-layout>
